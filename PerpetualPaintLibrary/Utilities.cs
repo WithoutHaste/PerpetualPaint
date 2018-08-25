dfa8f23d86928183e186df70be34a66201d2326f
J M <wohaste@gmail.com>
2018-08-25 15:17:17 -0600

    setup of color test cases

---
+++ PerpetualPaintLibrary/Utilities.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WithoutHaste.Drawing.Colors;
+
+namespace PerpetualPaintLibrary
+{
+    public static class Utilities
+    {
+		public static int MAX_RGB_AS_BLACK = 50;
+		public static int MIN_RGB_AS_WHITE = 230;
+
+		public static Color GrayscaleToColor(Color grayscale, Color newColor)
+		{
+			if(ColorIsPartiallyClear(grayscale))
+			{
+				grayscale = ConvertPartiallyClearToGray(grayscale);
+			}
+			if(ColorIsBlack(grayscale))
+			{
+				return grayscale;
+			}
+			if(ColorIsWhite(grayscale))
+			{
+				return newColor;
+			}
+			HSV grayscaleHSV = ConvertColors.HSVFromColor(grayscale);
+			HSV newColorHSV = ConvertColors.HSVFromColor(newColor);
+			float adjustedValue = grayscaleHSV.Value * newColorHSV.Value * 0.5f;
+			float adjustedSaturation = grayscaleHSV.Value * newColorHSV.Saturation * 0.5f; //cut adjusted saturation in half to force it into the gray range
+			HSV adjustedHSV = new HSV(newColorHSV.Hue, adjustedSaturation, adjustedValue);
+			Color adjustedColor = ConvertColors.ColorFromHSV(adjustedHSV);
+			return adjustedColor;
+		}
+
+		public static Color ColorToGrayscale(Color color, Color whiteColor)
+		{
+			if(ColorIsGrayscale(color))
+			{
+				return color;
+			}
+			if(color == whiteColor)
+			{
+				return Color.White;
+			}
+			HSV oldHSV = ConvertColors.HSVFromColor(color);
+			HSV oldWhite = ConvertColors.HSVFromColor(whiteColor);
+			float adjustedValue = oldHSV.Value / oldWhite.Value / 0.5f;
+			if(adjustedValue > 1)
+				adjustedValue = 1;
+			HSV adjustedHSV = new HSV(0, 0, adjustedValue);
+			Color adjustedColor = ConvertColors.ColorFromHSV(adjustedHSV);
+			return adjustedColor;
+		}
+
+		private static bool ColorIsClear(Color color)
+		{
+			return (color.A == 0);
+		}
+
+		private static bool ColorIsPartiallyClear(Color color)
+		{
+			return (color.A < 255);
+		}
+
+		private static bool ColorIsBlack(Color color)
+		{
+			if(ColorIsPartiallyClear(color)) return false;
+			return (ColorIsGrayscale(color) && color.R < MAX_RGB_AS_BLACK);
+		}
+
+		private static bool ColorIsGrayscale(Color color)
+		{
+			if(ColorIsPartiallyClear(color)) return true;
+			return (color.R == color.G && color.G == color.B);
+		}
+
+		private static bool ColorIsWhite(Color color)
+		{
+			if(ColorIsClear(color)) return true;
+			return (ColorIsGrayscale(color) && color.R > MIN_RGB_AS_WHITE);
+		}
+
+		private static Color ConvertPartiallyClearToGray(Color oldColor)
+		{
+			//25% solid => 75% gray
+			return ConvertColors.ColorFromHSV(0, 0, (255 - oldColor.A) / 255f);
+		}
+	}
+}
---
+++ PerpetualPaintTest/TestUtitilities.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PerpetualPaintLibrary;
+
+namespace PerpetualPaintTest
+{
+	[TestClass]
+	public class TestUtitilities
+	{
+		[TestMethod]
+		public void GenerateGradiantSamples()
+		{
+			List<Color> colors = new List<Color>() {
+				Color.Red,
+				Color.Orange,
+				Color.Yellow,
+				Color.Green,
+				Color.Blue,
+				Color.Purple
+			};
+			int width = 50;
+			foreach(Color color in colors)
+			{
+				Bitmap revertBitmap = GetRevertedGradient(width, color);
+				Bitmap colorBitmap = GetColoredGradient(width, color);
+				Bitmap grayBitmap = GetGrayscaleGradient(width);
+				Bitmap bitmap = new Bitmap(width * 3, revertBitmap.Height);
+				using(Graphics graphics = Graphics.FromImage(bitmap))
+				{
+					graphics.DrawImage(revertBitmap, new Point(0, 0));
+					graphics.DrawImage(colorBitmap, new Point(width, 0));
+					graphics.DrawImage(grayBitmap, new Point(width * 2, 0));
+				}
+				bitmap.Save(String.Format("GradientSamples/sample_{0:000}_{1:000}_{2:000}.png", color.R, color.G, color.B));
+			}
+		}
+
+		private Bitmap GetRevertedGradient(int width, Color color)
+		{
+			Bitmap bitmap = GetColoredGradient(width, color);
+			for(int x = 0; x < bitmap.Width; x++)
+			{
+				for(int y = 0; y < bitmap.Height; y++)
+				{
+					bitmap.SetPixel(x, y, Utilities.ColorToGrayscale(bitmap.GetPixel(x, y), color));
+				}
+			}
+			return bitmap;
+		}
+
+		private Bitmap GetColoredGradient(int width, Color color)
+		{
+			Bitmap bitmap = GetGrayscaleGradient(width);
+			for(int x = 0; x < bitmap.Width; x++)
+			{
+				for(int y = 0; y < bitmap.Height; y++)
+				{
+					bitmap.SetPixel(x, y, Utilities.GrayscaleToColor(bitmap.GetPixel(x, y), color));
+				}
+			}
+			return bitmap;
+		}
+
+		private Bitmap GetGrayscaleGradient(int width)
+		{
+			List<Color> grays = new List<Color>() {
+				Color.FromArgb(255, 0, 0, 0),
+				Color.FromArgb(255, 15, 15, 15),
+				Color.FromArgb(255, 31, 31, 31),
+				Color.FromArgb(255, 47, 47, 47),
+				Color.FromArgb(255, 63, 63, 63),
+				Color.FromArgb(255, 79, 79, 79),
+				Color.FromArgb(255, 95, 95, 95),
+				Color.FromArgb(255, 111, 111, 111),
+				Color.FromArgb(255, 127, 127, 127),
+				Color.FromArgb(255, 143, 143, 143),
+				Color.FromArgb(255, 159, 159, 159),
+				Color.FromArgb(255, 175, 175, 175),
+				Color.FromArgb(255, 191, 191, 191),
+				Color.FromArgb(255, 207, 207, 207),
+				Color.FromArgb(255, 223, 223, 223),
+				Color.FromArgb(255, 239, 239, 239),
+				Color.FromArgb(255, 255, 255, 255)
+			};
+			Bitmap bitmap = new Bitmap(width, width * grays.Count);
+			using(Graphics graphics = Graphics.FromImage(bitmap))
+			{
+				for(int i = 0; i < grays.Count; i++)
+				{
+					graphics.FillRectangle(new SolidBrush(grays[i]), new Rectangle(0, i * width, width, width));
+				}
+			}
+			return bitmap;
+		}
+	}
+}
