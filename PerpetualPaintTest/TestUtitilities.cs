45d79314b2ecd549a60c21f5618c0e4e3b31753a
J M <wohaste@gmail.com>
2018-08-26 09:25:20 -0600

    test cases for applying/removing color to grayscale

--- PerpetualPaintTest/TestUtitilities.cs
+++ PerpetualPaintTest/TestUtitilities.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Drawing;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using PerpetualPaintLibrary;
+using WithoutHaste.Drawing.Colors;
 
 namespace PerpetualPaintTest
 {
@@ -13,12 +14,136 @@ namespace PerpetualPaintTest
 		public void GenerateGradiantSamples()
 		{
 			List<Color> colors = new List<Color>() {
-				Color.Red,
-				Color.Orange,
-				Color.Yellow,
-				Color.Green,
-				Color.Blue,
-				Color.Purple
+				//100% saturation, 100% value
+				ConvertColors.ColorFromHSV(0, 1, 1), //red
+				ConvertColors.ColorFromHSV(30, 1, 1), //orange
+				ConvertColors.ColorFromHSV(60, 1, 1), //yellow
+				ConvertColors.ColorFromHSV(90, 1, 1), //green
+				ConvertColors.ColorFromHSV(120, 1, 1), //green also
+				ConvertColors.ColorFromHSV(150, 1, 1), //aqua
+				ConvertColors.ColorFromHSV(180, 1, 1), //cyan
+				ConvertColors.ColorFromHSV(210, 1, 1), //blue
+				ConvertColors.ColorFromHSV(240, 1, 1), //blue darker
+				ConvertColors.ColorFromHSV(270, 1, 1), //purple
+				ConvertColors.ColorFromHSV(300, 1, 1), //magenta
+				ConvertColors.ColorFromHSV(330, 1, 1), //fushia
+				//100% saturation, 50% value
+				ConvertColors.ColorFromHSV(0, 1, 0.5f), //red
+				ConvertColors.ColorFromHSV(30, 1, 0.5f), //orange
+				ConvertColors.ColorFromHSV(60, 1, 0.5f), //yellow
+				ConvertColors.ColorFromHSV(90, 1, 0.5f), //green
+				ConvertColors.ColorFromHSV(120, 1, 0.5f), //green also
+				ConvertColors.ColorFromHSV(150, 1, 0.5f), //aqua
+				ConvertColors.ColorFromHSV(180, 1, 0.5f), //cyan
+				ConvertColors.ColorFromHSV(210, 1, 0.5f), //blue
+				ConvertColors.ColorFromHSV(240, 1, 0.5f), //blue darker
+				ConvertColors.ColorFromHSV(270, 1, 0.5f), //purple
+				ConvertColors.ColorFromHSV(300, 1, 0.5f), //magenta
+				ConvertColors.ColorFromHSV(330, 1, 0.5f), //fushia
+				//100% saturation, 25% value
+				ConvertColors.ColorFromHSV(0, 1, 0.25f), //red
+				ConvertColors.ColorFromHSV(30, 1, 0.25f), //orange
+				ConvertColors.ColorFromHSV(60, 1, 0.25f), //yellow
+				ConvertColors.ColorFromHSV(90, 1, 0.25f), //green
+				ConvertColors.ColorFromHSV(120, 1, 0.25f), //green also
+				ConvertColors.ColorFromHSV(150, 1, 0.25f), //aqua
+				ConvertColors.ColorFromHSV(180, 1, 0.25f), //cyan
+				ConvertColors.ColorFromHSV(210, 1, 0.25f), //blue
+				ConvertColors.ColorFromHSV(240, 1, 0.25f), //blue darker
+				ConvertColors.ColorFromHSV(270, 1, 0.25f), //purple
+				ConvertColors.ColorFromHSV(300, 1, 0.25f), //magenta
+				ConvertColors.ColorFromHSV(330, 1, 0.25f), //fushia
+				//50% saturation, 100% value
+				ConvertColors.ColorFromHSV(0, 0.5f, 1), //red
+				ConvertColors.ColorFromHSV(30, 0.5f, 1), //orange
+				ConvertColors.ColorFromHSV(60, 0.5f, 1), //yellow
+				ConvertColors.ColorFromHSV(90, 0.5f, 1), //green
+				ConvertColors.ColorFromHSV(120, 0.5f, 1), //green also
+				ConvertColors.ColorFromHSV(150, 0.5f, 1), //aqua
+				ConvertColors.ColorFromHSV(180, 0.5f, 1), //cyan
+				ConvertColors.ColorFromHSV(210, 0.5f, 1), //blue
+				ConvertColors.ColorFromHSV(240, 0.5f, 1), //blue darker
+				ConvertColors.ColorFromHSV(270, 0.5f, 1), //purple
+				ConvertColors.ColorFromHSV(300, 0.5f, 1), //magenta
+				ConvertColors.ColorFromHSV(330, 0.5f, 1), //fushia
+				//50% saturation, 50% value
+				ConvertColors.ColorFromHSV(0, 0.5f, 0.5f), //red
+				ConvertColors.ColorFromHSV(30, 0.5f, 0.5f), //orange
+				ConvertColors.ColorFromHSV(60, 0.5f, 0.5f), //yellow
+				ConvertColors.ColorFromHSV(90, 0.5f, 0.5f), //green
+				ConvertColors.ColorFromHSV(120, 0.5f, 0.5f), //green also
+				ConvertColors.ColorFromHSV(150, 0.5f, 0.5f), //aqua
+				ConvertColors.ColorFromHSV(180, 0.5f, 0.5f), //cyan
+				ConvertColors.ColorFromHSV(210, 0.5f, 0.5f), //blue
+				ConvertColors.ColorFromHSV(240, 0.5f, 0.5f), //blue darker
+				ConvertColors.ColorFromHSV(270, 0.5f, 0.5f), //purple
+				ConvertColors.ColorFromHSV(300, 0.5f, 0.5f), //magenta
+				ConvertColors.ColorFromHSV(330, 0.5f, 0.5f), //fushia
+				//50% saturation, 50% value
+				ConvertColors.ColorFromHSV(0, 0.5f, 0.25f), //red
+				ConvertColors.ColorFromHSV(30, 0.5f, 0.25f), //orange
+				ConvertColors.ColorFromHSV(60, 0.5f, 0.25f), //yellow
+				ConvertColors.ColorFromHSV(90, 0.5f, 0.25f), //green
+				ConvertColors.ColorFromHSV(120, 0.5f, 0.25f), //green also
+				ConvertColors.ColorFromHSV(150, 0.5f, 0.25f), //aqua
+				ConvertColors.ColorFromHSV(180, 0.5f, 0.25f), //cyan
+				ConvertColors.ColorFromHSV(210, 0.5f, 0.25f), //blue
+				ConvertColors.ColorFromHSV(240, 0.5f, 0.25f), //blue darker
+				ConvertColors.ColorFromHSV(270, 0.5f, 0.25f), //purple
+				ConvertColors.ColorFromHSV(300, 0.5f, 0.25f), //magenta
+				ConvertColors.ColorFromHSV(330, 0.5f, 0.25f), //fushia
+				//25% saturation, 100% value
+				ConvertColors.ColorFromHSV(0, 0.25f, 1), //red
+				ConvertColors.ColorFromHSV(30, 0.25f, 1), //orange
+				ConvertColors.ColorFromHSV(60, 0.25f, 1), //yellow
+				ConvertColors.ColorFromHSV(90, 0.25f, 1), //green
+				ConvertColors.ColorFromHSV(120, 0.25f, 1), //green also
+				ConvertColors.ColorFromHSV(150, 0.25f, 1), //aqua
+				ConvertColors.ColorFromHSV(180, 0.25f, 1), //cyan
+				ConvertColors.ColorFromHSV(210, 0.25f, 1), //blue
+				ConvertColors.ColorFromHSV(240, 0.25f, 1), //blue darker
+				ConvertColors.ColorFromHSV(270, 0.25f, 1), //purple
+				ConvertColors.ColorFromHSV(300, 0.25f, 1), //magenta
+				ConvertColors.ColorFromHSV(330, 0.25f, 1), //fushia
+				//25% saturation, 75% value
+				ConvertColors.ColorFromHSV(0, 0.25f, 0.75f), //red
+				ConvertColors.ColorFromHSV(30, 0.25f, 0.75f), //orange
+				ConvertColors.ColorFromHSV(60, 0.25f, 0.75f), //yellow
+				ConvertColors.ColorFromHSV(90, 0.25f, 0.75f), //green
+				ConvertColors.ColorFromHSV(120, 0.25f, 0.75f), //green also
+				ConvertColors.ColorFromHSV(150, 0.25f, 0.75f), //aqua
+				ConvertColors.ColorFromHSV(180, 0.25f, 0.75f), //cyan
+				ConvertColors.ColorFromHSV(210, 0.25f, 0.75f), //blue
+				ConvertColors.ColorFromHSV(240, 0.25f, 0.75f), //blue darker
+				ConvertColors.ColorFromHSV(270, 0.25f, 0.75f), //purple
+				ConvertColors.ColorFromHSV(300, 0.25f, 0.75f), //magenta
+				ConvertColors.ColorFromHSV(330, 0.25f, 0.75f), //fushia
+				//25% saturation, 50% value
+				ConvertColors.ColorFromHSV(0, 0.25f, 0.5f), //red
+				ConvertColors.ColorFromHSV(30, 0.25f, 0.5f), //orange
+				ConvertColors.ColorFromHSV(60, 0.25f, 0.5f), //yellow
+				ConvertColors.ColorFromHSV(90, 0.25f, 0.5f), //green
+				ConvertColors.ColorFromHSV(120, 0.25f, 0.5f), //green also
+				ConvertColors.ColorFromHSV(150, 0.25f, 0.5f), //aqua
+				ConvertColors.ColorFromHSV(180, 0.25f, 0.5f), //cyan
+				ConvertColors.ColorFromHSV(210, 0.25f, 0.5f), //blue
+				ConvertColors.ColorFromHSV(240, 0.25f, 0.5f), //blue darker
+				ConvertColors.ColorFromHSV(270, 0.25f, 0.5f), //purple
+				ConvertColors.ColorFromHSV(300, 0.25f, 0.5f), //magenta
+				ConvertColors.ColorFromHSV(330, 0.25f, 0.5f), //fushia
+				//25% saturation, 25% value
+				ConvertColors.ColorFromHSV(0, 0.25f, 0.25f), //red
+				ConvertColors.ColorFromHSV(30, 0.25f, 0.25f), //orange
+				ConvertColors.ColorFromHSV(60, 0.25f, 0.25f), //yellow
+				ConvertColors.ColorFromHSV(90, 0.25f, 0.25f), //green
+				ConvertColors.ColorFromHSV(120, 0.25f, 0.25f), //green also
+				ConvertColors.ColorFromHSV(150, 0.25f, 0.25f), //aqua
+				ConvertColors.ColorFromHSV(180, 0.25f, 0.25f), //cyan
+				ConvertColors.ColorFromHSV(210, 0.25f, 0.25f), //blue
+				ConvertColors.ColorFromHSV(240, 0.25f, 0.25f), //blue darker
+				ConvertColors.ColorFromHSV(270, 0.25f, 0.25f), //purple
+				ConvertColors.ColorFromHSV(300, 0.25f, 0.25f), //magenta
+				ConvertColors.ColorFromHSV(330, 0.25f, 0.25f), //fushia
 			};
 			int width = 50;
 			foreach(Color color in colors)
